e75af3b08df884564c952dcbd285debe5be42477
domiszy610 <opos113@gmail.com>
2019-12-15 13:14:11 +0100

    1

---
+++ app/src/main/java/com/example/tits5/LoginActivity.kt
@@ -0,0 +1,78 @@
+package com.example.tits5
+
+import android.content.Intent
+//import android.support.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private var mAuth:FirebaseAuth? = null
+    private var database = FirebaseDatabase.getInstance()
+    private var myRef = database.reference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
+
+
+    }
+
+    fun buLoginEvent(view:View)
+    {
+        LoginToFirebase(etEmail.text.toString(),etPassword.text.toString())
+    }
+
+    fun LoginToFirebase(email:String,password:String)
+    {
+        mAuth!!.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful)
+                {
+                    var currentUser = mAuth!!.currentUser
+                    Toast.makeText(applicationContext,"Successful Login",Toast.LENGTH_SHORT).show()
+                    if (currentUser != null) {
+                        myRef.child("Users").child(splitString(currentUser.email.toString())).child("Request").setValue(currentUser.uid)
+                    }
+                    LoadMain()
+                }
+                else
+                {
+                    Toast.makeText(applicationContext,"Login Failed. Check Email and Password Again!",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        LoadMain()
+    }
+
+    fun LoadMain()
+    {
+        var currentUser = mAuth!!.currentUser
+        if (currentUser != null) {
+
+            var intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("Email", currentUser.email)
+            intent.putExtra("uid", currentUser.uid)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+            startActivity(intent)
+        }
+    }
+
+    fun splitString(str:String):String
+    {
+        var split = str.split("@")
+        return split[0]
+    }
+}
---
+++ app/src/main/java/com/example/tits5/Notifications.kt
@@ -0,0 +1,36 @@
+package com.example.tits5
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+//import android.support.v4.app.NotificationCompat
+import androidx.core.app.NotificationCompat
+
+class Notifications
+{
+    var notifyTag = "new request"
+    fun Notify(context:Context,message:String,number:Int)
+    {
+        var intent = Intent(context,LoginActivity::class.java)
+        var builder = NotificationCompat.Builder(context)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setContentTitle("New Request")
+            .setContentText(message)
+            .setNumber(number)
+            //.setSmallIcon(R.drawable.tictactoe)
+            .setContentIntent(PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_UPDATE_CURRENT))
+            .setAutoCancel(true)
+
+        var nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            nm.notify(notifyTag, 0, builder.build())
+        }
+        else
+        {
+            nm.notify(notifyTag.hashCode(), builder.build())
+        }
+    }
+}
